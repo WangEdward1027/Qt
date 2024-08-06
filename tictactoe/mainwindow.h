659e2126e892a2e807744a384d02a15c5cd3b52f
Edward <78650387+WangEdward1027@users.noreply.github.com>
2024-08-06 10:45:29 +0800

    井字棋

--- tictactoe/mainwindow.h
+++ tictactoe/mainwindow.h
@@ -4,6 +4,8 @@
 #include <QMainWindow>
 #include <QString>
 #include <QStack>
+#include <QChar>
+#include <QPushButton>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -38,12 +40,11 @@ private slots:
 
     void on_pushButton_clear_clicked();
 
-    void on_pushButton_clicked();
-
     void on_pushButton_undo_clicked();
 
 private:
     Ui::MainWindow *ui;
-    QStack<int> _stack;
+    QStack<int> _stack;      //为了undo悔棋
+    QChar _turn;     //为了判断下一次该谁下棋
 };
 #endif // MAINWINDOW_H
