0873054b6281088ef7f4582b7e96ebf95cc3a6b5
Edward <78650387+WangEdward1027@users.noreply.github.com>
2024-08-06 10:46:05 +0800

    一个栈QStack + 一个QChar 实现

--- tictactoe/mainwindow.cpp
+++ tictactoe/mainwindow.cpp
@@ -2,15 +2,14 @@
 #include "ui_mainwindow.h"
 #include <QDebug>
 
-#define first_turn 'O'//O为先手
-#define last_turn  'X'//X为后手
+#define first_turn 'O' //O为先手
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    _stack_turn.push(first_turn); //O为先手
+    _turn = first_turn;
 }
 
 MainWindow::~MainWindow()
@@ -18,225 +17,140 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-//定一个所有按钮的公有函数
-//void MainWindow::public_pushButtion_clicked(QPushButton * pbtn)
-//{
-//    if(ui->pbtn->text().isEmpty()){ //为空才能占领
-//        QChar turn;
-//        if(!_stack_turn.empty()){
-//            turn = _stack_turn.pop();
-//            if(turn == 'O'){
-//                _stack_turn.push('X');
-//            }else{
-//                _stack_turn.push('O');
-//            }
-//        }else{ //第一步:先手下
-//            turn = first_turn;
-//            _stack_turn.push(last_turn);
-//        }
-
-//        ui->pbtn->setText(turn);
-
-//        _stack.push(1);
-//    }
-//}
-
 void MainWindow::on_pushButton_1_clicked()
 {
-    if(ui->pushButton_1->text().isEmpty()){ //为空才能占领
-        QChar turn;
-        if(!_stack_turn.empty()){
-            turn = _stack_turn.pop();
-            if(turn == 'O'){
-                _stack_turn.push('X');
-            }else{
-                _stack_turn.push('O');
-            }
-        }else{ //第一步:先手下
-            turn = first_turn;
-            _stack_turn.push(last_turn);
-        }
+    if(!ui->pushButton_1->text().isEmpty())  return; //为空才能占领
 
-        ui->pushButton_1->setText(turn);
+    ui->pushButton_1->setText(_turn);
 
-        _stack.push(1);
+    //修改下次下棋的顺序
+    if(_turn == 'O'){
+        _turn = 'X';
+    }else{
+        _turn = 'O';
     }
+
+    _stack.push(1);
 }
 
 void MainWindow::on_pushButton_2_clicked()
 {
-    if(ui->pushButton_2->text().isEmpty()){
-        QChar turn;
-        if(!_stack_turn.empty()){
-            turn = _stack_turn.pop();
-            if(turn == 'O'){
-                _stack_turn.push('X');
-            }else{
-                _stack_turn.push('O');
-            }
-        }else{
-            turn = first_turn;
-            _stack_turn.push(last_turn);
-        }
+    if(!ui->pushButton_2->text().isEmpty())  return;
 
-        ui->pushButton_2->setText(turn);
+    ui->pushButton_2->setText(_turn);
 
-        _stack.push(2);
+    if(_turn == 'O'){
+        _turn = 'X';
+    }else{
+        _turn = 'O';
     }
+
+    _stack.push(2);
 }
 
 void MainWindow::on_pushButton_3_clicked()
 {
-    if(ui->pushButton_3->text().isEmpty()){
-        QChar turn;
-        if(!_stack_turn.empty()){
-            turn = _stack_turn.pop();
-            if(turn == 'O'){
-                _stack_turn.push('X');
-            }else{
-                _stack_turn.push('O');
-            }
-        }else{
-            turn = first_turn;
-            _stack_turn.push(last_turn);
-        }
+    if(!ui->pushButton_3->text().isEmpty())  return;
 
-        ui->pushButton_3->setText(turn);
+    ui->pushButton_3->setText(_turn);
 
-        _stack.push(3);
+    if(_turn == 'O'){
+        _turn = 'X';
+    }else{
+        _turn = 'O';
     }
+
+    _stack.push(3);
 }
 
 void MainWindow::on_pushButton_4_clicked()
 {
-    if(ui->pushButton_4->text().isEmpty()){
-        QChar turn;
-        if(!_stack_turn.empty()){
-            turn = _stack_turn.pop();
-            if(turn == 'O'){
-                _stack_turn.push('X');
-            }else{
-                _stack_turn.push('O');
-            }
-        }else{
-            turn = first_turn;
-            _stack_turn.push(last_turn);
-        }
+    if(!ui->pushButton_4->text().isEmpty())  return;
 
-        ui->pushButton_4->setText(turn);
+    ui->pushButton_4->setText(_turn);
 
-        _stack.push(4);
+    if(_turn == 'O'){
+        _turn = 'X';
+    }else{
+        _turn = 'O';
     }
+
+    _stack.push(4);
 }
 
 void MainWindow::on_pushButton_5_clicked()
 {
-    if(ui->pushButton_5->text().isEmpty()){
-        QChar turn;
-        if(!_stack_turn.empty()){
-            turn = _stack_turn.pop();
-            if(turn == 'O'){
-                _stack_turn.push('X');
-            }else{
-                _stack_turn.push('O');
-            }
-        }else{
-            turn = first_turn;
-            _stack_turn.push(last_turn);
-        }
+    if(!ui->pushButton_5->text().isEmpty())  return;
 
-        ui->pushButton_5->setText(turn);
+    ui->pushButton_5->setText(_turn);
 
-        _stack.push(5);
+    if(_turn == 'O'){
+        _turn = 'X';
+    }else{
+        _turn = 'O';
     }
+
+    _stack.push(5);
 }
 
 void MainWindow::on_pushButton_6_clicked()
 {
-    if(ui->pushButton_6->text().isEmpty()){
-        QChar turn;
-        if(!_stack_turn.empty()){
-            turn = _stack_turn.pop();
-            if(turn == 'O'){
-                _stack_turn.push('X');
-            }else{
-                _stack_turn.push('O');
-            }
-        }else{
-            turn = first_turn;
-            _stack_turn.push(last_turn);
-        }
+    if(!ui->pushButton_6->text().isEmpty())  return;
 
-        ui->pushButton_6->setText(turn);
+    ui->pushButton_6->setText(_turn);
 
-        _stack.push(6);
+    if(_turn == 'O'){
+        _turn = 'X';
+    }else{
+        _turn = 'O';
     }
+
+    _stack.push(6);
 }
 
 void MainWindow::on_pushButton_7_clicked()
 {
-    if(ui->pushButton_7->text().isEmpty()){
-        QChar turn;
-        if(!_stack_turn.empty()){
-            turn = _stack_turn.pop();
-            if(turn == 'O'){
-                _stack_turn.push('X');
-            }else{
-                _stack_turn.push('O');
-            }
-        }else{
-            turn = first_turn;
-            _stack_turn.push(last_turn);
-        }
+    if(!ui->pushButton_7->text().isEmpty())  return;
 
-        ui->pushButton_7->setText(turn);
+    ui->pushButton_7->setText(_turn);
 
-        _stack.push(7);
+    if(_turn == 'O'){
+        _turn = 'X';
+    }else{
+        _turn = 'O';
     }
+
+    _stack.push(7);
 }
 
 void MainWindow::on_pushButton_8_clicked()
 {
-    if(ui->pushButton_8->text().isEmpty()){
-        QChar turn;
-        if(!_stack_turn.empty()){
-            turn = _stack_turn.pop();
-            if(turn == 'O'){
-                _stack_turn.push('X');
-            }else{
-                _stack_turn.push('O');
-            }
-        }else{
-            turn = first_turn;
-            _stack_turn.push(last_turn);
-        }
+    if(!ui->pushButton_8->text().isEmpty())  return;
 
-        ui->pushButton_8->setText(turn);
+    ui->pushButton_8->setText(_turn);
 
-        _stack.push(8);
+    if(_turn == 'O'){
+        _turn = 'X';
+    }else{
+        _turn = 'O';
     }
+
+    _stack.push(8);
 }
 
 void MainWindow::on_pushButton_9_clicked()
 {
-    if(ui->pushButton_9->text().isEmpty()){
-        QChar turn;
-        if(!_stack_turn.empty()){
-            turn = _stack_turn.pop();
-            if(turn == 'O'){
-                _stack_turn.push('X');
-            }else{
-                _stack_turn.push('O');
-            }
-        }else{
-            turn = first_turn;
-            _stack_turn.push(last_turn);
-        }
+    if(!ui->pushButton_9->text().isEmpty())  return;
 
-        ui->pushButton_9->setText(turn);
+    ui->pushButton_9->setText(_turn);
 
-        _stack.push(9);
+    if(_turn == 'O'){
+        _turn = 'X';
+    }else{
+        _turn = 'O';
     }
+
+    _stack.push(9);
 }
 
 void MainWindow::on_pushButton_clear_clicked()
@@ -251,7 +165,7 @@ void MainWindow::on_pushButton_clear_clicked()
     ui->pushButton_8->setText("");
     ui->pushButton_9->setText("");
 
-    _stack_turn.clear();
+    _turn = first_turn;
 }
 
 void MainWindow::on_pushButton_undo_clicked()
