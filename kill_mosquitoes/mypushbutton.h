ab3fa75c9695dea01099c5a69212676e3a2c00ce
Edward <78650387+WangEdward1027@users.noreply.github.com>
2024-08-08 22:36:46 +0800

    打

---
+++ kill_mosquitoes/mypushbutton.h
@@ -0,0 +1,31 @@
+#ifndef MYPUSHBUTTON_H
+#define MYPUSHBUTTON_H
+
+#include <QWidget>
+#include <QLabel>
+#include <QDebug>
+#include <QMouseEvent>
+
+class MyPushButton : public QLabel
+{
+    Q_OBJECT
+public:
+    explicit MyPushButton(QWidget *parent = nullptr);
+    void mousePressEvent(QMouseEvent *ev) override
+    {
+        emit mySignal();
+    }
+signals:
+    void mySignal();
+public slots:
+    void mySlot(){
+        int randomX = std::rand() % 600;
+        int randomY = std::rand() % 400;
+
+        this->move(randomX, randomY);
+        qDebug() << "kill mos";
+    }
+};
+
+
+#endif // MYPUSHBUTTON_H
