a7707fd605036d9fce6434bc1931774917caaa58
Edward <78650387+WangEdward1027@users.noreply.github.com>
2024-09-02 15:33:07 +0800

    QTimer 定时器。加入模态对话框，发现不会阻塞主窗口QTimer的刷新。

--- QTimer/mainwindow.cpp
+++ QTimer/mainwindow.cpp
@@ -1,6 +1,11 @@
+//QTimer定时器
+//加入模态对话框，发现不会阻塞主窗口QTimer的刷新
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QTime>
+#include <QDialog>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
 : QMainWindow(parent)
@@ -8,7 +13,7 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     timer = new QTimer(this);
-    timer->setInterval(3000);
+    timer->setInterval(1000);
     timer->start();
     QObject::connect(timer, &QTimer::timeout, this, &MainWindow::showTime);
     showTime(); //先手动调用一次槽函数,以在刚开始时就显示时间
@@ -23,4 +28,17 @@ void MainWindow::showTime()
 {
     QString text = QString("current time is %1").arg(QTime::currentTime().toString());
     ui->textBrowser->setText(text);
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    //    QDialog dialog(this);
+    //    dialog.setWindowTitle("模态对话框");
+    //    dialog.exec();  //弹出模态对话框
+    QMessageBox::StandardButton btn = QMessageBox::information(
+                this,                             // 父窗口
+                "模态对话框",                       // 标题
+                "看看QTimer时间是否有卡住？",         // 消息内容
+                QMessageBox::Ok | QMessageBox::No // 按钮类型
+                );
 }
